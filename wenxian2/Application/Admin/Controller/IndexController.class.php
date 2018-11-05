5b17c57e6019d9187f445b1c6f2aa6484197c372
xiaoyu888888 <xiaoyu1423477717@163.com>
2018-11-05 14:04:28 +0800

    后台单图片上传类

--- wenxian2/Application/Admin/Controller/IndexController.class.php
+++ wenxian2/Application/Admin/Controller/IndexController.class.php
@@ -1,15 +1,51 @@
 <?php
 namespace Admin\Controller;
 use Think\Controller;
+
 class IndexController extends Controller {
 
+
+  //主页面
     public function index()
     {
+      
+      if(empty($_SESSION['name']))
+      {   session('name',null);
+          $this->success('请先登录', 'login');
+      }
+      else
+      {
+
+          $this->display();
+      }
+      
+    }
     
-       $this->display();
+    //重要活动
+    public function release()
+    {
+      if($_POST)
+      {
+
+
+          echo 1;die;
+
+      }
+      else
+      {
+        if(empty($_SESSION['name']))
+        {   session('name',null);
+            $this->success('请先登录', 'login');
+        }
+        else
+        {
+
+          $this->display();
+        }
+  
+      }
 
     }
-
     //后台登录
     public function Login()
     {
@@ -32,6 +68,9 @@ class IndexController extends Controller {
             if($username == $name && $password ==$pwd)
             {
 
+                session_start();
+                session("name",$name); 
+
                 $this->success('登录成功', 'index');
 
             }
@@ -53,7 +92,7 @@ class IndexController extends Controller {
     	}
     	else
     	{
-
+        
            
     		 $this->display('login');
 
@@ -61,8 +100,74 @@ class IndexController extends Controller {
 
     }
 
+     public function add()
+     {
 
+            $this->display('add');
 
+     }
 
+     public function add_do()
+     {
+
+          $file=$this->do_upload('file_name');
+          
+     }
+
+   public function do_upload($logo,$path='./public/Uploads')
+    {
+        $fileInfo=$_FILES['file_name'];
+
+        $maxSize=2097152;//允许的最大值
+
+        $allowExt=array('jpeg','jpg','png','gif','wbmp');//允许的类型
+
+        $flag=true;//检测是否为真实图片类型
+        //判断错误号
+
+        if ($fileInfo['error']==0) {
+          //判断上传文件的大小
+          if ($fileInfo['size']>$maxSize) exit('上传文件大小不符合规则');
+
+          $ext = strtolower(pathinfo($fileInfo['name'],PATHINFO_EXTENSION));
+          
+          if (!in_array($ext,$allowExt)) exit('非法文件类型');
+
+          if (!is_uploaded_file($fileInfo['tmp_name'])) exit('文件不是通过HTTP POST方式上传的');
+
+          //检测是否是真实图片类型
+          if ($flag)
+          {
+            if (@!getimagesize($fileInfo['tmp_name']))
+            {
+              exit('不是真正的图片类型');
+            }
+          }
+
+          if (!file_exists($path))
+          {
+            mkdir($path,0777,true);
+            chmod($path,0777);
+          }
+          //确保文件名唯一防止产生覆盖
+          $nuiName=md5(uniqid(microtime(true),true)).'.'.$ext;
+
+          $destination=$path.'/'.$nuiName;
+
+          if (@move_uploaded_file($fileInfo['tmp_name'], $destination))
+          {
+            return $destination;
+          }
+          else
+          {
+            return 'error';
+          }
+        }
+        else
+        { 
+          return false;
+          // $this->uploads_error($fileInfo['error']);//验证错误类型
+        }
+     }
 
 }
